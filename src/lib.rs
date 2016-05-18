3520a7ac7a04164cc35bbe40e7b290a10f78aabf
Matt Yaraskavitch <yaraskavitch.matt@gmail.com>
2016-05-18 18:33:08 -0400

    Changed more lines of files with incorrect URL

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![warn(missing_docs)]
 #![doc(html_logo_url = "https://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
        html_favicon_url = "https://www.rust-lang.org/favicon.ico",
-       html_root_url = "https://jarusk.github.io/Spork")]
+       html_root_url = "https://jarusk.github.io/spork")]
 //! Some sample documentation
 //!
 
